d38a7f4ff00ac71de5320486d78a776a6ab85708
Seungil <schoi0115@gmail.com>
2023-10-14 13:19:01 -0400

    delete photos

--- src/components/Page1.js
+++ src/components/Page1.js
@@ -1,19 +1,5 @@
 import "../App.css";
 import React, { useState } from "react";
-// import pic1 from './images/1.jpg'
-// import pic2 from './images/2.jpg'
-// import pic3 from './images/3.jpg'
-// import pic4 from './images/4.jpg'
-// import pic5 from './images/5.jpg'
-// import pic6 from './images/6.jpg'
-// import pic7 from './images/7.jpg'
-// import pic8 from './images/8.jpg'
-// import pic9 from './images/9.jpg'
-// import pic10 from './images/10.jpg'
-// import pic11 from './images/11.jpg'
-// import pic12 from './images/12.jpg'
-// import pic13 from './images/13.jpg'
-// import pic14 from './images/14.jpg'
 
 function Page1() {
 
